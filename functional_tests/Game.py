0f5a9e011ccde35f185f788abee36ae7d9858373
9dev <9devmail@gmail.com>
2016-03-04 13:00:29 +0100

    Created an FT to successful game creation.

--- functional_tests/Game.py
+++ functional_tests/Game.py
@@ -1,11 +1,36 @@
 from ._Base import BaseTestCase
 
+from main.models import SHIPS
+
 
 SILVER = 'rgba(192, 192, 192, 1)'
 GREEN = 'rgba(0, 128, 0, 1)'
 
 
 class TestStartGame(BaseTestCase):
+
+    def create_ship_part(self, x, y):
+        field = self.browser.find_element_by_id('id_field_{}_{}'.format(x, y))
+        field.click()
+
+    def assert_can_create_ships(self):
+        # Florence clicks on the buttons until she creates all the ships.
+        line = 0
+        for length, n in SHIPS.items():
+            for i in range(n):
+                for j in range(length):
+                    self.create_ship_part(line, j)
+                line += 1
+
+    def assert_ships_are_present(self):
+        # Florence makes sure she sees all her ships on her board.
+        line = 0
+        for length, n in SHIPS.items():
+            for i in range(n):
+                for j in range(length):
+                    field = self.browser.find_element_by_id('id_field_{}_{}'.format(line, j))
+                    self.assertEqual(field.value_of_css_property('background-color'), GREEN)
+                line += 1
 
     def test_cannot_start_with_too_few_ships(self):
         # Florence hits the homepage.
@@ -29,8 +54,33 @@ class TestStartGame(BaseTestCase):
         # Florence submits the board.
         self.browser.find_element_by_tag_name('form').submit()
 
-        # She is redirected to the same page.
+        # She is being redirected to the same page.
         self.assertEqual(self.browser.current_url, '{}/new'.format(self.live_server_url))
 
         # She sees an error.
         self.assertIn('Too few ships!', self.browser.page_source)
+
+    def test_can_start_new_game(self):
+        # Florence hits the homepage.
+        self.get('/')
+
+        # She clicks on a link to start a new game.
+        self.browser.find_element_by_link_text('New game').click()
+
+        # She is taken to a game creator.
+        self.assertEqual(self.browser.current_url, '{}/new'.format(self.live_server_url))
+
+        # She sees a board.
+        self.browser.find_element_by_id('id_board')
+
+        # She creates her ships.
+        self.assert_can_create_ships()
+
+        # Florence submits the board.
+        self.browser.find_element_by_tag_name('form').submit()
+
+        # She is being redirected to a page with actual game.
+        self.assertEqual(self.browser.current_url, '{}/game/1'.format(self.live_server_url))
+
+        # She sees her ships on her board.
+        self.assert_ships_are_present()
