6b2f2529b7aabbd398c0f912e64fb624f154aac0
9dev <9devmail@gmail.com>
2016-03-04 09:25:43 +0100

    Created basic fabfile.

---
+++ fabfile.py
@@ -0,0 +1,34 @@
+from contextlib import contextmanager
+from fabric.api import cd, local, prefix, shell_env
+
+
+VENV_PATH = '~/.virtualenvs/baseships'
+PYTHON_PATH = '/usr/bin/python3.4'
+SETTINGS_MODULE = 'baseships.settings'
+
+
+def install():
+    local('virtualenv -p {} {}'.format(PYTHON_PATH, VENV_PATH))
+
+    with _venv():
+        local('pip install -r requirements.txt')
+
+        _django('makemigrations')
+        _django('migrate')
+        _django('loaddata base.json')
+
+
+def runserver():
+    with _venv():
+        _django('runserver')
+
+
+def _django(command):
+    return local('python manage.py {}'.format(command))
+
+
+@contextmanager
+def _venv():
+    with shell_env(DJANGO_SETTINGS_MODULE=SETTINGS_MODULE):
+        with prefix('. %s/bin/activate' % VENV_PATH):
+            yield
