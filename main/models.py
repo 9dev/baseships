ffcd2d05e7cbf645abc1235f89494b8a60714c95
9dev <9devmail@gmail.com>
2016-03-04 12:59:54 +0100

    Defined number and length of ships.

--- main/models.py
+++ main/models.py
@@ -6,6 +6,14 @@ from django.db import models
 BOARD_SIZE = 10
 FIELDS_LENGTH = len(json.dumps([[0] * BOARD_SIZE] * BOARD_SIZE))
 
+SHIPS = {
+    5: 1,
+    4: 1,
+    3: 1,
+    2: 2,
+    1: 2,
+}
+
 
 class Board(models.Model):
     owner = models.ForeignKey('auth.User', null=True)
