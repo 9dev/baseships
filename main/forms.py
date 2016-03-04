3f57137a8c9207d48f01c97bcc6b3f7b64ff8fa8
9dev <9devmail@gmail.com>
2016-03-04 12:34:23 +0100

    Added form validation to new game creator.

--- main/forms.py
+++ main/forms.py
@@ -1,14 +1,20 @@
+import re
+
 from django import forms
 
 from main.models import BOARD_SIZE
 
 
 class NewGameForm(forms.Form):
-    fields = forms.CharField(max_length=BOARD_SIZE**2, widget=forms.HiddenInput(), initial='0000')
+    fields = forms.CharField(max_length=BOARD_SIZE**2, widget=forms.HiddenInput(), label="Board")
 
     def clean_fields(self):
         fields = self.cleaned_data['fields']
-        if not fields.isdigit():
+
+        if not re.match('^[01]*$', fields):
             raise forms.ValidationError('Incorrect data provided!')
+
+        if fields.count('1') < 2:
+            raise forms.ValidationError('Too few ships!')
 
         return [[fields[i+j] for j in range(BOARD_SIZE)] for i in range(BOARD_SIZE)]  # convert str to list
